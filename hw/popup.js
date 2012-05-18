2ac6e45e72bf376654573473e6f4f0b719ed2139
John Trammell <johntrammell@gmail.com>
2012-05-18 14:40:01 -0500

    added google analytics

--- hw/popup.js
+++ hw/popup.js
@@ -35,3 +35,16 @@ function constructImageURL(photo) {
       "_" + photo.getAttribute("secret") +
       "_s.jpg";
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-31808351-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
